13c6e19413bd6c6666559be386bb9a8740f6d3f2
Oluwatobi Adenekan <oluwatobi.adenekan@taxfix.de>
2022-09-19 00:41:58 +0200

    fix(todo): error when persisting todo

--- src/entities/todo.ts
+++ src/entities/todo.ts
@@ -1,8 +1,8 @@
-import { Column, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class Todo {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
